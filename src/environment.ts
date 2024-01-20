116413c2fe513a8cfab0c28701f5a46d667be9dd
Thiago Coutinho Sousa Silva <tcoutinhossilva@gmail.com>
2024-01-20 16:43:27 -0300

    PR: AWS

--- src/environment.ts
+++ src/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   //endpoint: 'http://localhost:8080'
   //endpoint: 'http://52.15.61.91:8080'
-  endpoint: 'https://52.15.61.91:443'
+  endpoint: 'https://52.15.61.91:8443'
 }
