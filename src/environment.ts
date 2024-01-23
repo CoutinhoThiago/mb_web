9657ae8621bfb921b1a624c469f7e19d92cdd9d2
Thiago Coutinho Sousa Silva <tcoutinhossilva@gmail.com>
2024-01-23 14:19:46 -0300

    PR; teste

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //endpoint: 'http://localhost:8080'
-  //endpoint: 'http://52.15.61.91:8080'
-  endpoint: 'https://52.15.61.91:8443'
+  //endpoint: 'https://localhost:8080'
+  endpoint: 'http://52.15.61.91:8080'
+  //endpoint: 'https://52.15.61.91:8443'
 }
